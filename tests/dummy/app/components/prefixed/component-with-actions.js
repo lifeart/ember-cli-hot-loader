59285a7b6740ed98835a87fa08dbe7f3fae85c7b
Alex Kanunnikov <lifeart92@gmail.com>
2018-09-18 20:03:31 +0300

    #97 reproduction

---
+++ tests/dummy/app/components/prefixed/component-with-actions.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import hbs from 'htmlbars-inline-precompile';
+import { get } from '@ember/object';
+
+export default Component.extend({
+  layout: hbs`
+    <a {{action 'myAction' }}>Some button2</a>
+  `,
+  actions: {
+    myAction() {
+      const theAction = get(this, 'componentAction');
+      if (typeof theAction === 'function') {
+        theAction();
+      } else {
+        const targetObject = get(this, 'targetObject');
+        if (targetObject && targetObject.sendAction) {
+          targetObject.sendAction('componentAction');
+        } else {
+          this.sendAction('componentAction'); // ember v2.12
+        }
+      }
+    }
+  }
+});
--- tests/dummy/app/resolver.js
+++ tests/dummy/app/resolver.js
@@ -1,4 +1,13 @@
 import Resolver from 'ember-resolver';
 import HotReloadMixin from 'ember-cli-hot-loader/mixins/hot-reload-resolver';
+import Mixin from '@ember/object/mixin';
 
-export default Resolver.extend(HotReloadMixin);
+const CustomHotReloadMixin = Mixin.create(HotReloadMixin, {
+  shouldExcludeComponent({name}) {
+	const excludedFromConfig = this._super(...arguments);
+    const isPrefixed = name.startsWith('prefixed/');
+    return excludedFromConfig || isPrefixed;
+  }
+});
+
+export default Resolver.extend(CustomHotReloadMixin);
